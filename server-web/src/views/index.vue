8ad2fc664001aeae1e380e29f229bab4b58a0475
yoyoyo <yoyoyo20240@gmail.com>
2025-03-25 21:16:03 +0800

    🚀 Release v1.0.0

--- server-web/src/views/index.vue
+++ server-web/src/views/index.vue
@@ -4,8 +4,8 @@
       <div class="info-container">
         <div class="project-info">
           <h1 class="project-title">Nodami — 自建 梯子节点，从未如此简单</h1>
-          <p class="project-description">由 Nodami 提供支持 —— 一个开源的代理服务端管理平台，支持自动部署和集中管理多服务器、多节点，兼容
-            Shadowsocks / Vmess / Hysteria 协议，提供稳定、安全、高性能的网络连接。</p>
+          <p class="project-description">Nodami是一个开源的代理服务端管理平台，支持自动部署和集中管理多服务器、多节点，兼容
+            Shadowsocks / Vmess / Hysteria 协议，专为简化自建梯子节点而设计，无需复杂命令，可视化操作实现</p>
 
           <div class="project-links">
             <a href="https://github.com/YoyoCrafts/Nodami.git" class="github-link" target="_blank">
