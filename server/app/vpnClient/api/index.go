a4b95ecd421a2a2eb5a7ac75ad1298a3cf8f67da
yoyoyo <yoyoyo20240@gmail.com>
2025-04-26 13:48:49 +0800

    🚀 Release v1.0.5

--- server/app/vpnClient/api/index.go
+++ server/app/vpnClient/api/index.go
@@ -28,5 +28,9 @@ func (c *vpnClientApi) Subscription(r *ghttp.Request) {
 		return
 	}
 	//encoded := base64.StdEncoding.EncodeToString(yamlData)
-	library.TextExit(r, string(yamlData))
+	gg := "#本订阅地址由开源项目 Nodami 搭建管理。\n\n"
+	gg += "#Nodami 是一款功能强大的多节点、多服务器管理面板，支持一键部署和管理多个代理服务器。通过该面板，您可以轻松搭建和管理如 Shadowsocks、Vmess、Vless、Trojan、Hysteria2、Socks5 等主流协议的节点。\n\n"
+	gg += "#Nodami 让服务器管理更加高效，简化了技术部署，适合个人、企业和开发者使用。\n\n"
+	gg += "#⭐ 欢迎访问我们的 GitHub 项目地址:https://github.com/YoyoCrafts/Nodami  如果您喜欢，记得点个 ⭐ Star 来支持我们！\n\n"
+	library.TextExit(r, gg+string(yamlData))
 }
